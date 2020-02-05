5585ddae3e9edd880a88c3f4f37d0ca7aeebab5c
Dinesh <dinesh.goud@indovations.in>
2020-02-05 17:54:15 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'api/v1/users/login',
+               registration: 'api/v1/users/signup'
+             },
+             controllers: {
+               sessions: 'api/v1/users/sessions',
+               registrations: 'api/v1/users/registrations'
+             }
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :users do
+        collection do
+        end
+      end
+
+      resources :relations do
+        collection do 
+        end
+      end
+      resources :tweets do
+        collection do
+        end
+      end
+      
+    end
+    end
+  end
